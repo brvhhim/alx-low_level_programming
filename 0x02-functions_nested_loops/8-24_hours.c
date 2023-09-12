eb6d150e1257b270a56f4bf5992391d3f729f194
brvhhim <a.bruhhim@gmail.com>
2023-09-12 07:41:20 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * jack_bauer(void): prints every minut of the day
+ * @h: represents hours
+ * @m: represents minutes
+ * Description: Prints time in the format of 24 hours (00:00)
+ */
+void jack_bauer(void)
+{
+	int h0, h1;
+	int m0, m1;
+
+	for (h0 = 0; h0 <= 2 ; h0++)
+	{
+		for (h1 = 0 ; h1 < 10 ; h1++)
+		{
+			for (m0 = 0 ; m0 < 6 ; m0++)
+			{
+				for (m1 = 0 ; m1 < 10 ; m1++)
+				{
+					_putchar('0' + h0);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m0);
+					_putchar('0' + m1);
+					_putchar('\n');
+					if (h0 == 2 && h1 == 3 && m0 == 5 && m1 == 9)
+					{
+						return;
+					}
+				}
+			}
+		}
+	}
+}
