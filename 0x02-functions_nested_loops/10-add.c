e0607462003ff6a9615065c1e09537ac6a32183f
brvhhim <a.bruhhim@gmail.com>
2023-09-12 08:33:26 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * add(int, int): adds two integers
+ * Description: adds two integers and returns the result
+ * Return: integer value of result
+ */
+int add(int a, int b)
+{
+       return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * times_table(void): prints the 9 times table
+ * description: prints the 9 times table starting with 0
+ * Return: void
+ */
+void times_table(void)
+{
+	int v
