e5a5df398e586a8b5433334ada5d1c2791599837
brvhhim <a.bruhhim@gmail.com>
2023-09-14 13:31:44 -0700

    6-lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,18 +9,10 @@
 
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0 ; i <= n ; i++)
+	while (!(n <= 0))
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
 		_putchar('_');
-		}
+		n--;
 	}
 	_putchar('\n');
 }
