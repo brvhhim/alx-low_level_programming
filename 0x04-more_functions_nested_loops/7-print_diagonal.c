09a99b6697682d75eca8eab247a18379ae49e816
brvhhim <a.bruhhim@gmail.com>
2023-09-14 13:44:37 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line to a given lenth.
+ * @n: length to print to.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0 ; i < n ; i++)
+	{
+		j = i;
+		while (j > 0)
+		{
+			_putchar(32);
+			j--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
+
