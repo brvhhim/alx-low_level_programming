30ac4907ec0fed5948cd324114ab20d838eab7df
brvhhim <a.bruhhim@gmail.com>
2023-09-21 03:43:43 -0700

    7-leet

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates 2 strings.
+ * _strncat - concatenate strings, defining the size of second string.
  * @dest: string with concatenation
  * @src: string to be concatenated
+ * @n: size of second string
  * Return: Always 0.
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int lengthD, lengthS;
 
@@ -17,7 +18,7 @@ char *_strcat(char *dest, char *src)
 	while (*(dest + lengthD) != '\0')
 		lengthD++;
 
-	while (*(src + lengthS) != '\0' && lengthD < 97)
+	while (*(src + lengthS) != '\0' && lengthD < 97 && lengthS < n)
 	{
 		*(dest + lengthD) = *(src + lengthS);
 		lengthD++;
